42ce79bda3c3798ffdd669d9a1c1c12a6492c6ef
gabry780 <58219192+gabry780@users.noreply.github.com>
2021-06-12 22:44:42 +0200

    Update Booking.java

--- Booking.java
+++ Booking.java
@@ -5,86 +5,51 @@ import User.Team;
 
 import java.util.ArrayList;
 
-/**
- * Booking class responsible for holding the booking information
- */
+
 public class Booking {
 
-    /**
-     * playground attribute for holding the playground that was booked
-     */
     private Playground playground;
-    /**
-     * player attribute for holding the player that booked the playground
-     */
+    
     private Player player;
-    /**
-     * time attribute for holding how long the playground is booked
-     */
+    
     private Time time;
-    /**
-     * teamMembers that are to be invited to the booked playground
-     */
+    
     private ArrayList<Team.TeamMember> teamMembers;
 
-    /**
-     * Booking constructor initiates the following:
-     * a new playground
-     * a new player
-     * a new time
-     * @param verifier takes verifier to use it to initialize the playground constructor
-     */
     public Booking(Verifier verifier) {
         playground = new Playground(verifier);
         player = new Player(verifier);
         time = new Time();
     }
 
-    /**
-     * @return booked playground
-     */
     public Playground getPlayground() {
         return this.playground;
     }
 
-    /**
-     * @param playground sets current booked playground with the value of playground
-     */
     public void setPlayground(Playground playground) {
         this.playground = playground;
     }
 
-    /**
-     * @return current player that booked the playground
-     */
+    
     public Player getPlayer() {
         return this.player;
     }
 
-    /**
-     * @param player sets current player that booked the playground
-     */
     public void setPlayer(Player player) {
         this.player = player;
     }
 
-    /**
-     * @return how long the playground is booked
-     */
+    
     public Time getTime() {
         return this.time;
     }
 
-    /**
-     * @param time sets the current time range with the value of time
-     */
+    
     public void setTime(Time time) {
         this.time = time;
     }
 
-    /**
-     * refunds player with money if the cancellation was within the cancellation period
-     */
+   
     public void refund() {
         Time currentTime = new Time();
         currentTime.setCurrentTime();
@@ -98,9 +63,7 @@ public class Booking {
         }
     }
 
-    /**
-     * @return the information of the booking such as the booking duration and the player
-     */
+    
     @Override
     public String toString() {
         return "Booking duration:\n" + time + '\n' +
